ad00a713c679f23e8a038822f01b46efe693225d
Abhinav Patil <apatil.cs@gmail.com>
2018-11-03 19:27:04 -0400

    Problem 10 (Scala)

---
+++ ScalaSolutions/0010.scala
@@ -0,0 +1,7 @@
+object Euler0010 {
+  val primes: Stream[Long] = 2L #:: Stream.from(3).map(_.toLong).filter(i => primes.takeWhile(j => j * j <= i).forall(k => i % k > 0))
+
+  def main(args: Array[String]): Unit = {
+    println(primes.takeWhile(_ < 2000000).sum)
+  }
+}
